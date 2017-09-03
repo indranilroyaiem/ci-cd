9da0d4d8542a6f3e0ffd0db80938c2a90a82d844
root <indranilroy.aiem@gmail.com>
2017-09-03 07:44:13 -0400

    new changes in test case

--- tests/unittest_dev.py
+++ tests/unittest_dev.py
@@ -4,17 +4,17 @@ from devops2 import devtest
 
 def test_sample_single_word():
     l = ('foo', 'bar', 'foobar')
-    word = generator.sample(l)
+    word = devtest.sample(l)
     assert word in l
 
 def test_sample_multiple_words():
     l = ('foo', 'bar', 'foobar')
-    words = generator.sample(l, 2)
+    words = devtest.sample(l, 2)
     assert len(words) == 2
     assert words[0] in l
     assert words[1] in l
     assert words[0] is not words[1]
 
 def test_generate_buzz_of_at_least_five_words():
-    phrase = generator.generate_buzz()
+    phrase = devtest.generate_buzz()
     assert len(phrase.split()) >= 5
